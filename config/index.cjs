b71d695fda614499e1aca7863c7bdd624acf3538
Jerry2022032 <103949691+Jerry2022032@users.noreply.github.com>
2022-11-29 16:40:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,6 +23,8 @@ const USER_CONFIG = {
       name: '大宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oz9Mm6CufO9nO4PUXl6xPR5JyQEM',
+      // 金
+      // id: 'oz9Mm6OMJLgpSYyc9ZrDvywLJI3U',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ztIjCazme_i3F2jhGXwjLZ6c2CnaWCIfv84guzBJ9LQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
